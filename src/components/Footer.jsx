3158ba5295eb017933250b0741a8961ebf87ac3d
David Johnson <davemoses3@gmail.com>
2022-08-01 10:13:40 +0100

    aminat ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
   const date = new Date();
 
   return (
-    <footer className="bg-[#800080] pt-16 pb-8">
-      <div className="container mx-auto px-5 py-16 flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col border-y-2 border-white">
+    <footer className="bg-[#800080] pt-16 pb-8 px-8">
+      <div className="container mx-auto md:px-5 py-16 flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col border-y-2 border-white">
         <div className="w-80 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
           <div className="space-y-2">
             <h1 className="text-3xl text-white">
@@ -14,7 +14,7 @@ const Footer = () => {
             </h1>
           </div>
         </div>
-        <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+        <div className="flex-grow flex flex-wrap md:pl-20 pl-0 mt-10 md:text-left text-center">
           <div className="lg:w-1/3 md:w-1/2 w-full px-4">
             <nav className="list-none mb-10 space-y-8">
               <li>
@@ -44,7 +44,7 @@ const Footer = () => {
             </nav>
           </div>
           <div className="lg:w-1/3 md:w-1/2 w-full px-4">
-            <nav className="list-none mb-10 space-y-8">
+            <nav className="list-none space-y-8">
               <li>
                 <Link to={"/"} className="text-white hover:text-gray-200">
                   Twitter
@@ -59,7 +59,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="flex justify-center items-center mt-10">
+      <div className="flex justify-center items-center mt-10 px-8 text-center md:text-sm text-xs">
         <p className="text-white">
           Copyright ©2022 - Aminat Akinode | All right reserved
         </p>
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -12,31 +12,32 @@ const ProjectList = ({
   return (
     <>
       {position === "left" ? (
-        <div class="flex pt-16 md:flex-row flex-col-reverse items-center md:gap-10 gap-0">
-          <div class="lg:max-w-lg lg:w-full md:w-1/2  flex flex-col md:items-start md:text-left items-center">
-            <p class="leading-relaxed mb-6">{subtitle}</p>
-            <h1 class="title-font sm:text-6xl text-3xl mb-6 font-medium text-neutral-700">
+       
+        <div className="flex pt-16 md:flex-row-reverse flex-col items-center md:gap-10 gap-0">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 mb-10 md:mb-0">
+            <div className="flex md:flex-row-reverse items-center md:flex-nowrap flex-wrap ">
+              <div className="md:rotate-90 whitespace-nowrap md:mb-0 mb-6 text-left">
+                {industry}
+              </div>
+              <img src={img} alt="" />
+            </div>
+          </div>
+          <div className="lg:flex-grow md:w-1/2  md:pl-16 flex flex-col md:items-start md:text-left items-center">
+            <p className="leading-relaxed mb-6">{subtitle}</p>
+            <h1 className="title-font sm:text-6xl text-3xl mb-6 font-medium text-neutral-700">
               {title}
             </h1>
-            <p class="mb-6 leading-relaxed text-gray-500">{type}</p>
+            <p className="mb-6 leading-relaxed text-gray-500">{type}</p>
             <a
               href={link}
-              class="mb-8 leading-relaxed font-bold cursor-pointer text-[#800080]">
+              className="mb-8 leading-relaxed font-bold cursor-pointer text-[#800080]">
               Case Study
             </a>
           </div>
-          <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16">
-            <div className="flex md:flex-row flex-col-reverse mb-10 md:mb-0   md:flex-nowrap flex-wrap ">
-              <img src={img} alt="" />
-              <div className="md:rotate-90 whitespace-nowrap md:mb-0 mb-6">
-                {industry}
-              </div>
-            </div>
-          </div>
         </div>
       ) : (
-        <div class="flex pt-16 md:flex-row flex-col items-center md:gap-10 gap-0">
-          <div class="lg:max-w-lg lg:w-full md:w-1/2 mb-10 md:mb-0">
+        <div className="flex pt-16 md:flex-row flex-col items-center md:gap-10 gap-0">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 mb-10 md:mb-0">
             <div className="flex items-center md:flex-nowrap flex-wrap ">
               <div className="md:rotate-90 whitespace-nowrap md:mb-0 mb-6">
                 {industry}
@@ -44,15 +45,15 @@ const ProjectList = ({
               <img src={img} alt="" />
             </div>
           </div>
-          <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center">
-            <p class="leading-relaxed mb-6">{subtitle}</p>
-            <h1 class="title-font sm:text-6xl text-3xl mb-6 font-medium text-neutral-700">
+          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center">
+            <p className="leading-relaxed mb-6">{subtitle}</p>
+            <h1 className="title-font sm:text-6xl text-3xl mb-6 font-medium text-neutral-700">
               {title}
             </h1>
-            <p class="mb-6 leading-relaxed text-gray-500">{type}</p>
+            <p className="mb-6 leading-relaxed text-gray-500">{type}</p>
             <a
               href={link}
-              class="mb-8 leading-relaxed font-bold cursor-pointer text-[#800080]">
+              className="mb-8 leading-relaxed font-bold cursor-pointer text-[#800080]">
               Case Study
             </a>
           </div>
