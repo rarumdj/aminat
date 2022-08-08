86c47e8bbac087f323293cf11b68ecc22de1f26c
David Johnson <davemoses3@gmail.com>
2022-08-08 19:54:39 +0100

    fix: img

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,16 +1,15 @@
 export { default as aminat } from "./aminat.png";
 export { default as colaborate } from "./colaborate.svg";
-export { default as communication } from "./communication.svg";;
+export { default as communication } from "./communication.svg";
 export { default as heroimg } from "./hero.svg";
 export { default as learning } from "./learning.svg";
 export { default as lines } from "./lines.svg";
 export { default as logoBlue } from "./logo-blue.svg";
 export { default as logo } from "./logo.svg";
-export { default as presentationFour } from "./presentationfour.jpeg";
-export { default as presentationOne } from "./presentationone.jpeg";
-export { default as presentationThree } from "./presentationthree.jpeg";
-export { default as presentationTwo } from "./presentationtwo.jpeg";
+export { default as presentationFour } from "./presentationfour.jpg";
+export { default as presentationOne } from "./presentationone.jpg";
+export { default as presentationThree } from "./presentationthree.jpg";
+export { default as presentationTwo } from "./presentationtwo.jpg";
 export { default as solving } from "./solving.svg";
 export { default as bubble } from "./bubble.gif";
 export { default as bubblepattern } from "./patternbubble.svg";
-
